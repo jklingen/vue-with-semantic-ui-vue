adfdd3824fd39ff3e9877e1b8856e67c05df1931
jklingen <github@jklingen.com>
2019-04-30 13:48:21 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import SuiVue from 'semantic-ui-vue';
+import '../semantic/dist/semantic.min.css';
+
+Vue.config.productionTip = false
+
+Vue.use(SuiVue);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
